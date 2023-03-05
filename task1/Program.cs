9d50c6693194c34f4ca648ec4d57cb507923054f
TheUserOne <theuserone@yandex.ru>
2023-03-05 16:43:17 +0500

    tasks are complited

---
+++ task1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void Array(double[] array)
+{
+    Console.WriteLine("Enter number of array (press enter after each of them)");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("Your numbers: ");
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write($"{array[i]}");
+        i++;
+        if (i < array.Length)
+        {
+            Console.Write($", ");
+        }
+        else
+        {
+            Console.Write($".");
+        }
+    }
+}
+
+void TotalPos(double[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+        else
+        {
+            count = count;
+        }
+    }
+    Console.WriteLine($"");
+    Console.Write($"Total of numbers greater than zero is {count}");
+}
+
+
+Console.WriteLine("Enter number of elements of array");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[n];
+
+Array(array);
+PrintArray(array);
+TotalPos(array);
---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double CrossPoint(double k, double m, double b, double c)
+{
+    double x = (c - b) / (k - m);
+    if (k == m)
+    {
+        Console.WriteLine("The lines are parallel");
+    }
+    else
+    {
+        x = (c - b) / (k - m);
+        Console.WriteLine(x);
+    }
+    return x;
+}
+
+
+
+Console.WriteLine("Enter first numbers (press enter after each of them)");
+double k = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second numbers (press enter after each of them)");
+double m = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+
+CrossPoint(k, m, b, c);
